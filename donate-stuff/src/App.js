1306c789e993d2421044430a7a5eb9fb6f3eedd3
BartlomiejKedziora <70581658+BartlomiejKedziora@users.noreply.github.com>
2020-12-15 10:58:19 +0100

    basic config done

--- donate-stuff/src/App.js
+++ donate-stuff/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="test">Testowy text sprawdzajacy colory i czcionki</p>
+        <p className="test2">A nawet DWA</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
