f79f1639c378978261e04043d9742731db94b993
BartlomiejKedziora <70581658+BartlomiejKedziora@users.noreply.github.com>
2020-12-16 20:53:18 +0100

    all styling is done

--- donate-stuff/src/components/Home/Banner/Banner.js
+++ donate-stuff/src/components/Home/Banner/Banner.js
@@ -3,15 +3,17 @@ import {Link, Router} from 'react-router-dom';
 
 const Banner = () => {
     return(
-        <header className="header container">
-            <h1 id="banner" className="header-title">Zacznij pomagać!</h1>
-            <h2 className="header-title">Oddaj niechciane rzeczy w zaufane ręce</h2>
-            <div className="header-decor"></div>
-            <div className="header-cta">
-                <Link to="/signin" className="header-cta-btn">Oddaj rzeczy</Link>
-                <Link to="/signin" className="header-cta-btn">Zorganizuj zbiórkę</Link>
-            </div>
-            
+        <header className="header-hero container">
+            <div className="header-bckg"></div>
+            <div className="header">
+                <h1 id="banner" className="header-title">Zacznij pomagać!</h1>
+                <h2 className="header-title">Oddaj niechciane rzeczy w zaufane ręce</h2>
+                <div className="header-decor"></div>
+                <div className="header-cta">
+                    <Link to="/signin" className="header-cta-btn">Oddaj rzeczy</Link>
+                    <Link to="/signin" className="header-cta-btn">Zorganizuj zbiórkę</Link>
+                </div>
+            </div>   
         </header>
     );
 }
