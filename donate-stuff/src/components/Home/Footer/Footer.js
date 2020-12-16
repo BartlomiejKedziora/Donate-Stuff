b04614a3dee28c4465941220e352022782fa03bc
BartlomiejKedziora <70581658+BartlomiejKedziora@users.noreply.github.com>
2020-12-16 17:49:12 +0100

    mobile first done

--- donate-stuff/src/components/Home/Footer/Footer.js
+++ donate-stuff/src/components/Home/Footer/Footer.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Footer = () => {
     return(
         <>
-            <div className="footer container">
+            <footer className="footer container">
                 <small className="footer-rights">Copyright by Coders Lab</small>
                 <div className="footer-social">
                     <div className="footer-social-icon footer-fb"></div>
                     <div className="footer-social-icon footer-insta"></div>
                 </div>
-            </div>
+            </footer>
         </>
     );
 }
--- donate-stuff/src/components/Home/Receivers/Receivers.js
+++ donate-stuff/src/components/Home/Receivers/Receivers.js
@@ -3,7 +3,40 @@ import React from 'react';
 const Receivers = () => {
     return(
         <>
-            <div id="receivers">Organizacje</div>
+            <section id="receivers" className="receivers container">
+                <h2 className="receivers-title">Komu pomagamy?</h2>
+                <div className="receivers-decor"></div>
+                <div className="receivers-groups">
+                    <p className="receivers-groups-item">Fundacjom</p>
+                    <p className="receivers-groups-item">Organizacjom pozarządowym</p>
+                    <p className="receivers-groups-item">Lokalnym zbiórkom</p>
+                </div>
+                <p div className="receivers-text">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
+                <div className="receivers-list">
+                    <div className="receivers-list-item">
+                        <div className="receivers-list-item-left">
+                            <p className="receivers-list-item-name">Organizacja "Trala la"</p>
+                            <p className="receivers-list-item-info">Zbieracz, handlarz, kombajnista</p>
+                        </div>
+                        <p className="receivers-list-item-right">ul. Rynek 10 , Kraków</p>
+                    </div>
+                    <div className="receivers-list-item">
+                        <div className="receivers-list-item-left">
+                            <p className="receivers-list-item-name">Organizacja "Trala la"</p>
+                            <p className="receivers-list-item-info">Zbieracz, handlarz, kombajnista</p>
+                        </div>
+                        <p className="receivers-list-item-right">ul. Rynek 10 , Kraków</p>
+                    </div>
+                    <div className="receivers-list-item">
+                        <div className="receivers-list-item-left">
+                            <p className="receivers-list-item-name">Organizacja "Trala la"</p>
+                            <p className="receivers-list-item-info">Zbieracz, handlarz, kombajnista</p>
+                        </div>
+                        <p className="receivers-list-item-right">ul. Rynek 10 , Kraków</p>
+                    </div>
+                </div>
+                <div className="receivers-list-number">1</div>
+            </section>
         </>
     );
 }
