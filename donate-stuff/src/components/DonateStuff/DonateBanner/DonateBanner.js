7c3a21c0cb19f3a4057d7b818bb1ff61f3af28be
BartlomiejKedziora <70581658+BartlomiejKedziora@users.noreply.github.com>
2021-01-05 10:42:07 +0100

    donate-stuff banner, orange styling

--- donate-stuff/src/components/DonateStuff/DonateBanner/DonateBanner.js
+++ donate-stuff/src/components/DonateStuff/DonateBanner/DonateBanner.js
@@ -4,13 +4,17 @@ import {Link} from 'react-router-dom';
 const DonateBanner = () => {
     return(
         <header className="header-hero container">
-            <div className="header-bckg"></div>
+            <div className="header-bckg header-bckg-donate"></div>
             <div className="header">
-                <h1 id="banner" className="header-title">Zacznij pomagać!</h1>
-                <h2 className="header-title">Oddaj niechciane rzeczy w zaufane ręce</h2>
+                <h1 id="banner" className="header-title">Oddaj rzeczy, których już nie chcesz</h1>
+                <h2 className="header-title">POTRZEBUJĄCYM</h2>
                 <div className="header-decor"></div>
-                <div className="header-cta">
-                    Wystarczą 4 proste kroki
+                <div className="header-subtitle">Wystarczą 4 proste kroki: </div>
+                <div className="header-sqrs">
+                    <div className="header-sqrs-item"><p className="header-sqrs-txt">1<br></br><br></br>Wybierz rzeczy</p></div>
+                    <div className="header-sqrs-item"><p className="header-sqrs-txt">2<br></br><br></br>Spakuj je w worki</p></div>
+                    <div className="header-sqrs-item"><p className="header-sqrs-txt">3<br></br><br></br>Wybierz fundację</p></div>
+                    <div className="header-sqrs-item"><p className="header-sqrs-txt">4<br></br><br></br>Zamów kuriera</p></div>
                 </div>
             </div>   
         </header>
--- donate-stuff/src/components/DonateStuff/DonateOrange/DonateOrange.js
+++ donate-stuff/src/components/DonateStuff/DonateOrange/DonateOrange.js
@@ -4,9 +4,9 @@ const DonateOrange = () => {
     return(
         <>
             <section className="orange container">
-                <article className="orange-article">
-                    <p className="orange-title">Ważne!</p>
-                    <p className="orange-text">Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
+                <article className="donate-orange-article">
+                    <p className="donate-orange-title">Ważne!</p>
+                    <p className="donate-orange-text">Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
                 </article>
             </section>
         </>
